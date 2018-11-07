b202adec2d1c4a9882d1bb3c17b61b32fe28a6ea
Murat Seymen <seymenmurat16@hotmail.com>
2018-11-08 02:46:55 +0300

    face recognation ve face detection eklendi

---
+++ FaceRecognation/base.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+
+# webcam için
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+     ret, frame = cap.read()
+
+     cv2.imshow('frame',frame)
+     if cv2.waitKey(20) & 0xFF == ord('q'):
+         break
+
+cap.release()
+cv2.destroyAllWindows()
