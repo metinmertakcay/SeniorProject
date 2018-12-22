99d21328c848cacd9c803e88344993c32e93361a
Murat Seymen <seymenmurat16@hotmail.com>
2018-12-22 05:06:11 +0300

    demo_news teki memory hatası düzeltildi

--- VideoClassification/demo_news.py
+++ VideoClassification/demo_news.py
@@ -17,8 +17,9 @@ import os
 import imageio
 os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
-def predict(data_type, seq_length, saved_model, image_shape, video_name, class_limit):
-    model = load_model(saved_model)
+def predict(data_type, seq_length, model, image_shape, video_name, class_limit):
+    """ her seferinde bu fonksiyonda modeli yüklüyordu artık direk yüklenmiş modeli yolluyorum fonksiyona"""
+    # model = load_model(saved_model)
 
     # Get the data and process it.
     if image_shape is None:
@@ -60,7 +61,7 @@ def main():
     # TODO Make this way more useful. It should take in the path to
     # an actual video file, extract frames, generate sequences, etc.
     #video_name = 'v_Archery_g04_c02'
-    video = 'v_Deneme_g16'
+    video = 'v_Deneme_g28'
     video_class = "z"
     videos = glob.glob(os.path.join('data', 'test', video_class, video + '_c*.avi'))
 
@@ -74,13 +75,17 @@ def main():
     else:
         raise ValueError("Invalid model. See train.py for options.")
 
-    # Videonun kaç fps olacağını öğrenmek için ilk videodaki fps değerini alıyorum
+    """ Videonun kaç fps olacağını öğrenmek için ilk videodaki fps değerini alıyorum """
     reader = imageio.get_reader(videos[0])
     fps = reader.get_meta_data()['fps']
     # Video yazıcıyı oluşturuyorum
     writer = imageio.get_writer(video + ".avi", fps=fps)
     reader.close()
     # Tüm ayırılmış videoları geziyorum
+    """ modelli burda yüklüyorum ve aşağıdaki predict fonksiyonuna saved model yerine bu modeli gönderiyorum"""
+    """ bu arada son chapteri bulamıyor bunun nedeni de son chapter 40 frameden az olduğu için o chapterin özellikleri
+        çıkarılmıyor o yüzden hata verip sonlanıyor ama video yinede oluşuyor sadece son chapter yok """
+    model = load_model(saved_model)
     for item in videos:
         # İtemi ayırıp sadece video isminini alıyorum
         news = item.split("\\")
@@ -88,7 +93,7 @@ def main():
         parts = news_name.split(".")
         video_name = parts[0]
         # Tahmin yapılıyor
-        predict_label = predict(data_type, seq_length, saved_model, image_shape, video_name, class_limit)
+        predict_label = predict(data_type, seq_length, model, image_shape, video_name, class_limit)
         # Tahmin edilen videoya tahmin sonucunu yazdırıyorum
         reader = imageio.get_reader(item)
         for i, frame in enumerate(reader):
